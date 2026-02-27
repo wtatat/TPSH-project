0d9313380eef0f4261b0b0145554bdec62c30057
Qefoz <daniilder01@gmail.com>
2026-02-27 20:47:11 +1000

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,35 +1,31 @@
 CREATE TABLE IF NOT EXISTS videos (
-    id text PRIMARY KEY,
-    creator_id text NOT NULL,
-    video_created_at timestamptz NOT NULL,
-    views_count bigint NOT NULL,
-    likes_count bigint NOT NULL,
-    comments_count bigint NOT NULL,
-    reports_count bigint NOT NULL,
-    created_at timestamptz NOT NULL,
-    updated_at timestamptz NOT NULL
+    id TEXT PRIMARY KEY,
+    creator_id TEXT NOT NULL,
+    video_created_at TIMESTAMPTZ NOT NULL,
+    views_count BIGINT NOT NULL,
+    likes_count BIGINT NOT NULL,
+    comments_count BIGINT NOT NULL,
+    reports_count BIGINT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS video_snapshots (
-    id text PRIMARY KEY,
-    video_id text NOT NULL REFERENCES videos(id) ON DELETE CASCADE,
-    views_count bigint NOT NULL,
-    likes_count bigint NOT NULL,
-    comments_count bigint NOT NULL,
-    reports_count bigint NOT NULL,
-    delta_views_count bigint NOT NULL,
-    delta_likes_count bigint NOT NULL,
-    delta_comments_count bigint NOT NULL,
-    delta_reports_count bigint NOT NULL,
-    created_at timestamptz NOT NULL,
-    updated_at timestamptz NOT NULL
+    id TEXT PRIMARY KEY,
+    video_id TEXT NOT NULL REFERENCES videos(id) ON DELETE CASCADE,
+    views_count BIGINT NOT NULL,
+    likes_count BIGINT NOT NULL,
+    comments_count BIGINT NOT NULL,
+    reports_count BIGINT NOT NULL,
+    delta_views_count BIGINT NOT NULL,
+    delta_likes_count BIGINT NOT NULL,
+    delta_comments_count BIGINT NOT NULL,
+    delta_reports_count BIGINT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
 );
 
-CREATE INDEX IF NOT EXISTS idx_videos_creator_id ON videos(creator_id);
-CREATE INDEX IF NOT EXISTS idx_videos_video_created_at ON videos(video_created_at);
-CREATE INDEX IF NOT EXISTS idx_videos_views_count ON videos(views_count);
-
-CREATE INDEX IF NOT EXISTS idx_video_snapshots_video_id ON video_snapshots(video_id);
-CREATE INDEX IF NOT EXISTS idx_video_snapshots_created_at ON video_snapshots(created_at);
-CREATE INDEX IF NOT EXISTS idx_video_snapshots_delta_views_count ON video_snapshots(delta_views_count);
-CREATE INDEX IF NOT EXISTS idx_video_snapshots_delta_likes_count ON video_snapshots(delta_likes_count);
+CREATE INDEX IF NOT EXISTS idx_videos_creator_id ON videos (creator_id);
+CREATE INDEX IF NOT EXISTS idx_videos_video_created_at ON videos (video_created_at);
+CREATE INDEX IF NOT EXISTS idx_video_snapshots_video_id ON video_snapshots (video_id);
+CREATE INDEX IF NOT EXISTS idx_video_snapshots_created_at ON video_snapshots (created_at);
